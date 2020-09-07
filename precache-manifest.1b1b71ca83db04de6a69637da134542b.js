4b03ba8ee4476f2fc7a6b0b324cd317ded611488
Ponseelan <ponseelan.a@pogeyan.com>
2020-09-07 18:32:19 +0530

    Updates

---
+++ precache-manifest.1b1b71ca83db04de6a69637da134542b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d019dc0a43ee0a6f73f3bff045b0363",
+    "url": "/excel-viewer/index.html"
+  },
+  {
+    "revision": "fe6bfc5aa5f319a76829",
+    "url": "/excel-viewer/static/css/main.4fda3b7c.chunk.css"
+  },
+  {
+    "revision": "e8326609df6a38a47cfe",
+    "url": "/excel-viewer/static/js/2.7530def6.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/excel-viewer/static/js/2.7530def6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6bfc5aa5f319a76829",
+    "url": "/excel-viewer/static/js/main.da3f8fc8.chunk.js"
+  },
+  {
+    "revision": "a1dd8530d72954d0c027",
+    "url": "/excel-viewer/static/js/runtime-main.a06976de.js"
+  }
+]);
